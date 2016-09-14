8826b094bd7286c145bc725895b5b71203d1e2e3
rajeshd50 <rajeshdan50@gmail.com>
2016-09-14 14:28:44 +0530

    state update example

--- public_html/App.jsx
+++ public_html/App.jsx
@@ -1,73 +1,55 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 
 class App extends React.Component {
    constructor() {
-   	super();
-   	this.state = {
-         data: 
-         [
-            {
-               "id":1,
-               "name":"Foo",
-               "age":"20"
-            },
-				
-            {
-               "id":2,
-               "name":"Bar",
-               "age":"30"
-            },
-				
-            {
-               "id":3,
-               "name":"Baz",
-               "age":"40"
-            }
-         ]
+      super();
+      
+      this.state = {
+         data: []
       }
+   
+      this.setStateHandler = this.setStateHandler.bind(this);
+      this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+      this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
+   };
+
+   setStateHandler() {
+      var item = "setState..."
+      var myArray = this.state.data;
+      myArray.push(item)
+      this.setState({data: myArray})
+   };
+
+   forceUpdateHandler() {
+      this.forceUpdate();
    }
+
+   findDomNodeHandler() {
+      var colorArr = ['green','red','blue','black','yellow'];
+      var myDiv = document.getElementById('myDiv');
+      ReactDOM.findDOMNode(myDiv).style.color = colorArr[Math.floor(Math.random()*colorArr.length)];
+   }
+
    render() {
+      let myUl = {
+         "listStyle": "none"
+      };
+      let myLi = {
+         "background": "#AAA",
+         "margin": "5px 0"
+      };
       return (
          <div>
-            <Header/>
-            <table>
-            	<thead>
-            		<tr><th>ID</th><th>Name</th><th>Age</th></tr>
-            	</thead>
-            	<tbody>
-            		{this.state.data.map((person,i) => <TableRow key={i} data={person}/> )}
-            	</tbody>
-            </table>
+            <button onClick = {this.setStateHandler}>SET STATE</button>
+            <button onClick = {this.forceUpdateHandler}>Force Update</button>
+            <button onClick = {this.findDomNodeHandler}>FIND DOME NODE</button>
+            <div id="myDiv">{Math.random()}</div>
+            <ul style={myUl}>
+               {this.state.data.map((obj,i)=> <li style={myLi} key={i}>{obj}</li>)}
+            </ul>
          </div>
-      );
-   }
-}
-
-class Header extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Header</h1>
-         </div>
-      );
-   }
-}
-
-class TableRow extends React.Component {
-   render() {
-      return (
-         <tr>
-            <td>{this.props.data.id}</td>
-            <td>{this.props.data.name}</td>
-            <td>{this.props.data.age}</td>
-         </tr>
       );
    }
 }

585d267f8a0f365843831f246989476539d60f5f
rajeshd50 <rajeshdan50@gmail.com>
2016-09-14 14:40:28 +0530

    component life cycle

--- public_html/App.jsx
+++ public_html/App.jsx
@@ -1,73 +1,66 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import React from 'react';
 
 class App extends React.Component {
-   constructor() {
-   	super();
-   	this.state = {
-         data: 
-         [
-            {
-               "id":1,
-               "name":"Foo",
-               "age":"20"
-            },
-				
-            {
-               "id":2,
-               "name":"Bar",
-               "age":"30"
-            },
-				
-            {
-               "id":3,
-               "name":"Baz",
-               "age":"40"
-            }
-         ]
+
+   constructor(props) {
+      super(props);
+      
+      this.state = {
+         data: 0
       }
+
+      this.setNewNumber = this.setNewNumber.bind(this)
+   };
+
+   setNewNumber() {
+      this.setState({data: this.state.data + 1})
    }
+
    render() {
       return (
          <div>
-            <Header/>
-            <table>
-            	<thead>
-            		<tr><th>ID</th><th>Name</th><th>Age</th></tr>
-            	</thead>
-            	<tbody>
-            		{this.state.data.map((person,i) => <TableRow key={i} data={person}/> )}
-            	</tbody>
-            </table>
+            <button onClick = {this.setNewNumber}>INCREMENT</button>
+            <Content myNumber = {this.state.data}></Content>
          </div>
       );
    }
 }
 
-class Header extends React.Component {
+class Content extends React.Component {
+
+   componentWillMount() {
+      console.log('Component WILL MOUNT!')
+   }
+
+   componentDidMount() {
+      console.log('Component DID MOUNT!')
+   }
+
+   componentWillReceiveProps(newProps) {    
+      console.log('Component WILL RECIEVE PROPS!')
+   }
+
+   shouldComponentUpdate(newProps, newState) {
+      return true;
+   }
+
+   componentWillUpdate(nextProps, nextState) {
+      console.log('Component WILL UPDATE!');
+   }
+
+   componentDidUpdate(prevProps, prevState) {
+      console.log('Component DID UPDATE!')
+   }
+
+   componentWillUnmount() {
+      console.log('Component WILL UNMOUNT!')
+   }
+   
    render() {
       return (
          <div>
-            <h1>Header</h1>
+            <h3>{this.props.myNumber}</h3>
          </div>
-      );
-   }
-}
-
-class TableRow extends React.Component {
-   render() {
-      return (
-         <tr>
-            <td>{this.props.data.id}</td>
-            <td>{this.props.data.name}</td>
-            <td>{this.props.data.age}</td>
-         </tr>
       );
    }
 }
